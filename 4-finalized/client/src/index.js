4ee46513a61eb36a6c0f3798ea80c1afdd8490ab
Edgar Coime <59620774+edgarcoime@users.noreply.github.com>
2022-03-31 21:34:38 -0700

    feat: applying styles

--- 4-finalized/client/src/index.js
+++ 4-finalized/client/src/index.js
@@ -1,9 +1,10 @@
-import * as _ from "lodash";
+import "./index.css";
+// import * as _ from "lodash";
 
 console.log("Hello world!");
 
-let myArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// let myArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-export function chunkifyArray(chunkSize) {
-  return _.chunk(myArr, chunkSize);
-}
+// export function chunkifyArray(chunkSize) {
+//   return _.chunk(myArr, chunkSize);
+// }
