3ebac56b4152c83a795e23aefb35ac05dc6d78a6
Edgar Coime <59620774+edgarcoime@users.noreply.github.com>
2022-04-01 00:14:14 -0700

    feat: remove comments

--- 1-the_problem/4-whatABundlerDoes.js
+++ 1-the_problem/4-whatABundlerDoes.js
@@ -39,30 +39,3 @@ var foo = require("foo");
 var bar = require("bar");
 const { nodeName } = require("jquery");
 foo.helloWorld();
-
-
-// Sample output of a very simple javascript bunder tool 
-
-// 4 main Advantages:
-
-// 1. We can now use npm 
-//   - Because require()/exports are implemented in the same way in node.js
-//   - All the bundler needs is look up the dependency in node_modules and inject into the
-//     bundle the code that you need. 
-//   - This allows the use of npm's powerful versioning system
-
-// 2. Allows us to write cross platform code
-//   - require() behaves the same in node.js or in browser environment. 
-//   - As long as you don't depend on specific functionality in the browser or node.js core modules
-//   - If you do then you can always just check to see what platform you are in using `typeof window !== "undefined"`
-
-// 3. Allows us to use different syntax
-//   - allows the use of latest version of javascript syntax like ES6 or ES5
-//   - we don't have to worry about our code not working in other browsers since we can transpile into common.js 
-
-// 4. Allows minification or even the removal of unused code
-//   - Some bundlers allow for minification of code to create a smaller js bundle size
-//   - Can even remove dead or unused code. 
-
-// 5. Can even allow for code splitting for many other types of code
-//   - Allows us to modularize css, images, fonts, and even wasm
